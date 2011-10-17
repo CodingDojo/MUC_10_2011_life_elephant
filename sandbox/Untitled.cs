08aaec85c93217db7e1c451d04ac7e324cfdbccf
root <root@rails.(none)>
2011-10-17 21:13:36 +0000

    3

--- sandbox/Untitled.cs
+++ sandbox/Untitled.cs
@@ -2,8 +2,16 @@ using System;
 
 public class Game
 {
+    private bool[,] originalArray;
+    private int width;
+    private int height;
+
+
     public string NextGeneration(string input)
     {
+        string[] lines = input.Split('\n');
         return "..\n..";
     }
+
+    
 }
